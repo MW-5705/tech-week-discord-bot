733e11b2547839e86e80e6d8e6e9453d12abda5e
MW-5705 <113458487+MW-5705@users.noreply.github.com>
2024-06-28 19:35:39 +0530

    modified channels.py

--- cogs/channels.py
+++ cogs/channels.py
@@ -13,11 +13,14 @@ class Channels(commands.Cog):
     @commands.command()
     @commands.has_role("ExBo")
     async def channel(self, ctx):
+        print("chala to sahi")
         guild = ctx.guild
         db = client["Discord_bot"]
         collection = db["Teams"]
+        collection_channels = db["Channels"]
         teams_data = collection.find({},{"_id":0})
         teams_list = []
+        teams_channels = []
         for team in teams_data:
             teams_list.append(team)
         for team in teams_list:
@@ -28,9 +31,19 @@ class Channels(commands.Cog):
             for i in members:
                 member = guild.get_member(i)
                 overwrites[member] = discord.PermissionOverwrite(read_messages = True)
-            await guild.create_text_channel(name = team["team_id"], overwrites = overwrites)
-            await guild.create_voice_channel(name = team["team_id"], overwrites = overwrites)
+            team_id = team["team_id"]
+            text_channel = await guild.create_text_channel(name = f"{team_id}_text", overwrites = overwrites)
+            voice_channel = await guild.create_voice_channel(name = f"{team_id}_voice", overwrites = overwrites)
+            
+            print(type(text_channel))
+            print(type(voice_channel))
+            channel = {"team_id" : team_id, "text_channel": text_channel.id, "voice_channel" : voice_channel.id}
+            collection_channels.insert_one(channel)
         await ctx.send("Channels created")
+   
+   
+        
+        
 
 async def setup(client):
     await client.add_cog(Channels(client))
